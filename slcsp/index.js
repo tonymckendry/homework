f3ce379953d7330ea04a72331cce87e9786032cb
Tony McKendry <tonymckendry@gmail.com>
2018-02-27 09:40:47 -0700

    lets go to work

--- slcsp/index.js
+++ slcsp/index.js
@@ -1,7 +1,27 @@
-var csvParse = require('csv-parse')
+var fs = require('fs')
+var parse = require('csv-parse')
+var async = require('async')
+var _ = require('lodash')
+require('require-csv')
+let theCSV = require('./csv_files/plans.csv')
 
+// var test = function() {
+//     var inputFile = './csv_files/plans.csv'
+
+//     var parser = parse({ delimiter: ',' }, function(err, data) {
+//         let headers = data[0]
+//     })
+//     fs.createReadStream(inputFile).pipe(parser)
+// }
 var test = function() {
-    console.log('test')
+    var headers = theCSV[0]
+    theCSV.splice(0, 1) // remove the header line
+    populatedObject = {}
+    theCSV.forEach(function(item) {
+        var obj = _.zipObject(headers, item)
+        populatedObject[obj.plan_id] = obj //not sure what key we will need but this is how
+    })
+    console.log(populatedObject)
 }
 
 test()
