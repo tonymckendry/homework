fdb090963b984a89f746e596911d894ccac4aceb
Tony McKendry <tonymckendry@gmail.com>
2018-02-27 18:02:57 -0700

    state object done

--- slcsp/index.js
+++ slcsp/index.js
@@ -1,27 +1,42 @@
-var fs = require('fs')
-var parse = require('csv-parse')
-var async = require('async')
-var _ = require('lodash')
+const fs = require('fs')
+const parse = require('csv-parse')
+const async = require('async')
+const _ = require('lodash')
 require('require-csv')
-let theCSV = require('./csv_files/plans.csv')
+const plans = require('./csv_files/plans.csv')
+const stateCodes = require('./states').states
 
-// var test = function() {
-//     var inputFile = './csv_files/plans.csv'
-
-//     var parser = parse({ delimiter: ',' }, function(err, data) {
-//         let headers = data[0]
-//     })
-//     fs.createReadStream(inputFile).pipe(parser)
-// }
-var test = function() {
-    var headers = theCSV[0]
-    theCSV.splice(0, 1) // remove the header line
-    populatedObject = {}
-    theCSV.forEach(function(item) {
-        var obj = _.zipObject(headers, item)
-        populatedObject[obj.plan_id] = obj //not sure what key we will need but this is how
-    })
-    console.log(populatedObject)
+const findSLCSPByZip = function() {
+    let statePlans = buildStatePlans()
+    let all
 }
 
-test()
+const buildStatePlans = () => {
+    const headers = plans[0]
+    plans.splice(0, 1) // remove the header line
+    let states = {}
+    stateCodes.forEach(code => {
+        states[code] = {}
+    })
+    plans.forEach(row => {
+        var item = _.zipObject(headers, row)
+        let stateRateArea = states[item.state][item.rate_area]
+        if (stateRateArea) {
+            stateRateArea[item.metal_level].push({ rate: item.rate, planId: item.plan_id })
+        } else {
+            states[item.state][item.rate_area] = {
+                Bronze: [],
+                Silver: [],
+                Gold: [],
+                Platinum: [],
+                Catastrophic: []
+            }
+            states[item.state][item.rate_area][item.metal_level].push({ rate: item.rate, planId: item.plan_id })
+        }
+    })
+    return states
+}
+
+const buildZips = () => {}
+
+findSLCSPByZip()
